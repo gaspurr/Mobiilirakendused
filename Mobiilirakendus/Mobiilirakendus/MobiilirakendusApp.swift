d96523955cf0e9ce8ba28214587a592d18af2b9f
gaspurr <luik.gaspar@gmail.com>
2022-02-25 13:33:55 +0200

    Init

---
+++ Mobiilirakendus/Mobiilirakendus/MobiilirakendusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobiilirakendusApp.swift
+//  Mobiilirakendus
+//
+//  Created by Gaspar Luik on 25.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MobiilirakendusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
