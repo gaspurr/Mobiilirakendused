c4ef62c9e93977e354ea7cc447b87eb3a443ffc7
Johannes Theodor Kollist <johmar@tlu.ee>
2022-03-01 20:18:29 +0200

    Väiksed muudatused pluss Icon
    Contentview buttoni lisamine ja random number-i genereerimine.

--- Mobiilirakendus/Mobiilirakendus/ContentView.swift
+++ Mobiilirakendus/Mobiilirakendus/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var nr = 0
+    let url = "https://loveincorporated.blob.core.windows.net/contentimages/gallery/03211459-0607-4d07-8a6c-9966e3820a7d-Mount-Kirkjufell-Iceland.jpg"
     var body: some View {
         
         VStack{
-            Image("cold")
-                .resizable()
-                .scaledToFit()
+//            Image("cold")
+//                .resizable()
+//                .scaledToFit()
+            AsyncImage(url: URL(string: url)) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 200, height: 200)
+                .background(Color.gray)
+                .clipShape(Circle())
             Text("What a nice weather")
                 .font(.largeTitle)
                 .bold()
@@ -21,8 +33,22 @@ struct ContentView: View {
             Text("Currently: 2 °C. Light rain. Mostly cloudy.")
                 .font(.title)
                 .foregroundColor(.secondary)
+            Text("Random number is:\(nr)")
+            Button(action: {
+                nr=Int.random(in: 1..<10)
+            }){
+                HStack{
+                    Image("summer")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    Text("Generate number")
+                }
+                .background(Color.blue)
+                .cornerRadius(10)
+                .foregroundColor(Color.white)
+            }
         }
-
+        
     }
 }
 
