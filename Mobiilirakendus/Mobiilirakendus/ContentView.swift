c33307b420d3d4d863d04c6c8adf82e7b3d417ac
gaspurr <luik.gaspar@gmail.com>
2022-03-14 15:55:52 +0200

    Task 2 done
    Added a navigation view to the first view and a navigation link that directs to the second view

--- Mobiilirakendus/Mobiilirakendus/ContentView.swift
+++ Mobiilirakendus/Mobiilirakendus/ContentView.swift
@@ -11,44 +11,58 @@ struct ContentView: View {
     @State var nr = 0
     let url = "https://loveincorporated.blob.core.windows.net/contentimages/gallery/03211459-0607-4d07-8a6c-9966e3820a7d-Mount-Kirkjufell-Iceland.jpg"
     var body: some View {
+        //Navigation view tähendab, et selle ala sees saab niiöedal lehte vahetada. Kui Navigation view oleks näiteks selline ->
+        /*NavigationView{
         
-        VStack{
-//            Image("cold")
-//                .resizable()
-//                .scaledToFit()
-            AsyncImage(url: URL(string: url)) { image in
-                    image
-                        .resizable()
-                        .scaledToFill()
-                } placeholder: {
-                    ProgressView()
+            VStack{
+                NavigationLink(destination: SecondView()){
+                    Text("Second view")
                 }
-                .frame(width: 200, height: 200)
-                .background(Color.gray)
-                .clipShape(Circle())
-            Text("What a nice weather")
-                .font(.largeTitle)
-                .bold()
-                .multilineTextAlignment(.center)
-            Text("Currently: 2 °C. Light rain. Mostly cloudy.")
-                .font(.title)
-                .foregroundColor(.secondary)
-            Text("Random number is:\(nr)")
-            Button(action: {
-                nr=Int.random(in: 1..<10)
-            }){
-                HStack{
-                    Image("summer")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                    Text("Generate number")
+         }*/
+        // siis katab vahetuv leht ainult pool ekraanist ära, sest ta ei hõlma tervet ekraani.
+        NavigationView{
+        
+            VStack{
+                //navigationlink on siis niiöelda <a> tag html-is, see redirectib uuele vaatele
+                NavigationLink(destination: SecondView()){
+                    //Siia sisse võib panna pildi, nupu, mida iganes
+                    Text("Second view")
                 }
-                .background(Color.blue)
-                .cornerRadius(10)
-                .foregroundColor(Color.white)
+    //            Image("
+                AsyncImage(url: URL(string: url)) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 200, height: 200)
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                Text("What a nice weather")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                Text("Currently: 2 °C. Light rain. Mostly cloudy.")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                Text("Random number is: \(nr)")
+                Button(action: {
+                    nr=Int.random(in: 1..<10)
+                }){
+                    HStack{
+                        Image("summer")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Text("Generate number")
+                    }
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(Color.white)
+                }
             }
+            
         }
-        
     }
 }
 
