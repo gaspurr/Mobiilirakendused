56689a906e7f91f5ecf81583a568943864ae76f1
gaspurr <luik.gaspar@gmail.com>
2022-04-03 18:57:59 +0300

    conflict resolve merge

--- Mobiilirakendus/Mobiilirakendus/ContentView.swift
+++ Mobiilirakendus/Mobiilirakendus/ContentView.swift
@@ -69,45 +69,43 @@ struct ContentView: View {
                         Text("About you")
                     }
                 case 2:
-                    VStack{
-                        Spacer()
-                //            Image("
-                        AsyncImage(url: URL(string: url)) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                            } placeholder: {
-                                ProgressView()
-                            }
-                            .frame(width: 200, height: 200)
-                            .background(Color.gray)
-                            .clipShape(Circle())
-                        Text("What a nice weather")
-                            .font(.largeTitle)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                        Text("Currently: 2 °C. Light rain. Mostly cloudy.")
-                            .font(.title)
-                            .foregroundColor(.secondary)
-                        Text("Random number is: \(nr)")
-                        Button(action: {
-                            nr=Int.random(in: 1..<10)
-                        }){
-                            HStack{
-                                Image("summer")
-                                    .resizable()
-                                    .frame(width: 50, height: 50)
-                                Text("Generate number")
-                            }
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                            .foregroundColor(Color.white)
-                        }
-                        Spacer()
-                    }
+                    Text("S")
  
                 default:
-                    Text("Remaining tabs")
+                    VStack{
+                        Spacer()
+                    AsyncImage(url: URL(string: url)) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 200, height: 200)
+                        .background(Color.gray)
+                        .clipShape(Circle())
+                    Text("What a nice weather")
+                        .font(.largeTitle)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                    Text("Currently: 2 °C. Light rain. Mostly cloudy.")
+                        .font(.title)
+                        .foregroundColor(.secondary)
+                    Text("Random number is: \(nr)")
+                    Button(action: {
+                        nr=Int.random(in: 1..<10)
+                    }){
+                        HStack{
+                            Image("summer")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                            Text("Generate number")
+                        }
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .foregroundColor(Color.white)
+                    }
+                }
                 }
             }
             
@@ -140,60 +138,13 @@ struct ContentView: View {
                     })
                 }
             }
-
-        }
-        
-    
-    
-}
-        NavigationView{
-        
-            VStack{
-                //navigationlink on siis niiöelda <a> tag html-is, see redirectib uuele vaatele
-                NavigationLink(destination: SecondView()){
-                    //Siia sisse võib panna pildi, nupu, mida iganes
-                    Text("Second view")
-                }
-    //            Image("
-                AsyncImage(url: URL(string: url)) { image in
-                        image
-                            .resizable()
-                            .scaledToFill()
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    .frame(width: 200, height: 200)
-                    .background(Color.gray)
-                    .clipShape(Circle())
-                Text("What a nice weather")
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                Text("Currently: 2 °C. Light rain. Mostly cloudy.")
-                    .font(.title)
-                    .foregroundColor(.secondary)
-                Text("Random number is: \(nr)")
-                Button(action: {
-                    nr=Int.random(in: 1..<10)
-                }){
-                    HStack{
-                        Image("summer")
-                            .resizable()
-                            .frame(width: 50, height: 50)
-                        Text("Generate number")
-                    }
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .foregroundColor(Color.white)
-                }
-            }
             
         }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
